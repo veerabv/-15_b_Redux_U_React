060a7d2b63779ae470615271bbe645dd857dfb41
veerabv <bveera0608@gmail.com>
2024-07-22 12:39:06 +0530

    dispatch a action and update the store and reflect the value in ui

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,28 @@
 import classes from "./Counter.module.css";
-import { useSelector } from "react-redux"; // this will directly give subscription to the store for this function and give acess to the component
+import { useSelector , useDispatch } from "react-redux"; // useSelector will directly give subscription to the store for this function and give acess to the component
+
 
 const Counter = () => {
+  const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
   const toggleCounterHandler = () => {};
+  const incHandler = () => {
+      dispatch({type:"INC"})
+  }
+  const decHandler = () => {
+    dispatch({type:"DEC"})
+  }
+  
+
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incHandler}>Increment</button>
+        <button onClick={decHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
