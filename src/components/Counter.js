144648375000e5fa065c19ab899baf2b808943aa
veerabv <bveera0608@gmail.com>
2024-07-22 12:30:34 +0530

    acess the value from the store using useSelector

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,14 @@
-import classes from './Counter.module.css';
+import classes from "./Counter.module.css";
+import { useSelector } from "react-redux"; // this will directly give subscription to the store for this function and give acess to the component
 
 const Counter = () => {
+  const counter = useSelector((state) => state.counter);
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
