d2413a559d27dc9d2a0a22a13991cc80dd4d06aa
veerabv <bveera0608@gmail.com>
2024-07-22 15:19:40 +0530

    create a slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,39 +1,53 @@
 // const redux = require("redux");
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {counter : 0 , isCounter : true}
+const initialState = { counter: 0, isCounter: true };
+
+const counterSlice = createSlice({ name: "counter", initialState: initialState, reducers: {
+    increment(state) {
+        state.counter++; // in toolkit we can mutate the state directly because the reduxtoolKit uses another package internally which take the copy of the state
+    },
+    decrement(state) {
+        state.counter--;
+    },
+    increase(state , action) {
+        state.counter = state.counter + action.amount
+    },
+    toggle(state) {
+        state.isCounter = !state.isCounter
+    }
+} });
 
 const counterReducer = (state = initialState, action) => {
   switch (action.type) {
     case "INC":
       return {
-        isCounter : state.isCounter,
-        counter: state.counter + 1
+        isCounter: state.isCounter,
+        counter: state.counter + 1,
       };
     case "DEC":
       return {
         counter: state.counter - 1,
-        isCounter : state.isCounter
+        isCounter: state.isCounter,
       };
 
-      case "INCREASE":
-        return {
-            isCounter : state.isCounter,
-            counter : state.counter + action.amount
-        }
+    case "INCREASE":
+      return {
+        isCounter: state.isCounter,
+        counter: state.counter + action.amount,
+      };
 
-        case "TOGGLE":
-            return {
-                counter : state.counter,
-                isCounter : !state.isCounter
-            }
+    case "TOGGLE":
+      return {
+        counter: state.counter,
+        isCounter: !state.isCounter,
+      };
     default:
-        return state;
+      return state;
   }
 };
 
 const store = createStore(counterReducer);
 
 export default store;
-
-
